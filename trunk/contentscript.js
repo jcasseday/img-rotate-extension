62516d75d0c0977cc7304f5e78807373c51ec0af
abdul2000@gmail.com <abdul2000@gmail.com@5c71e214-9e17-7a57-94e1-b0fcdf2311e2>
2011-05-09 12:37:16 +0000

    added CCW rotation

--- trunk/contentscript.js
+++ trunk/contentscript.js
@@ -1,30 +1,33 @@
 var domEl = null;
-$("img", document.body).mousedown(function (e) {
-    e.stopPropagation();
-    switch (e.which) {
-        case 3:
-            domEl = $(this).get(0);
-            break;
-        default:
-            break;
-    }
-});
+if (document.body.childElementCount === 1) {
+    domEl = $(document.images[0]).get(0);
+} else {
+    $("img", document.body).mousedown(function (e) {
+        e.stopPropagation();
+        switch (e.which) {
+            case 3:
+                domEl = $(this).get(0);
+                break;
+            default:
+                break;
+        }
+    });
+}
 
 chrome.extension.onRequest.addListener(onRequest);
 
 function onRequest(request, sender, sendResponse) {
-    var ang = getCurrentRotation(domEl);
+    var ang = getCurrentRotation(request.angle);
     $(domEl).rotate({angle:ang});
     sendResponse({}); // clean up.
 }
 
-function getCurrentRotation(domElement) {
-    var angle = 90;
-    var current = domElement.style['-webkit-transform'];
-    if (current) {
+function getCurrentRotation(angle) {
+    var current = domEl.style['-webkit-transform'];
+    if (current && current != "") {
         var beg = current.indexOf('(');
         var end = current.indexOf('deg)');
-        angle = angle + parseInt(current.substring(beg + 1, end));
+        angle = parseInt(current.substring(beg + 1, end)) + angle;
     }
     return angle;
 }
