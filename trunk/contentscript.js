13507f285f5d2a6c8122a205e94ff7c8a6b08f71
abdul2000@gmail.com <abdul2000@gmail.com@5c71e214-9e17-7a57-94e1-b0fcdf2311e2>
2011-05-09 12:44:36 +0000

    added ability to rotate image when viewed by itself on a page

--- trunk/contentscript.js
+++ trunk/contentscript.js
@@ -1,12 +1,13 @@
-var domEl = null;
+var selectedImage = null;
+//If the image is loaded by itself on the web page
 if (document.body.childElementCount === 1) {
-    domEl = $(document.images[0]).get(0);
+    selectedImage = $(document.images[0]).get(0);
 } else {
-    $("img", document.body).mousedown(function (e) {
-        e.stopPropagation();
-        switch (e.which) {
+    $("img", document.body).mousedown(function (event) {
+        event.stopPropagation();
+        switch (event.which) {
             case 3:
-                domEl = $(this).get(0);
+                selectedImage = $(this).get(0);
                 break;
             default:
                 break;
@@ -17,17 +18,17 @@ if (document.body.childElementCount === 1) {
 chrome.extension.onRequest.addListener(onRequest);
 
 function onRequest(request, sender, sendResponse) {
-    var ang = getCurrentRotation(request.angle);
-    $(domEl).rotate({angle:ang});
+    var degree = getCurrentRotation(request.angle);
+    selectedImage.style.webkitTransform = 'rotate(' + degree + 'deg)';
     sendResponse({}); // clean up.
 }
 
 function getCurrentRotation(angle) {
-    var current = domEl.style['-webkit-transform'];
-    if (current && current != "") {
-        var beg = current.indexOf('(');
-        var end = current.indexOf('deg)');
-        angle = parseInt(current.substring(beg + 1, end)) + angle;
+    var currentDegree = selectedImage.style['-webkit-transform'];
+    if (currentDegree && currentDegree != "") {
+        var start = currentDegree.indexOf('(');
+        var end = currentDegree.indexOf('deg)');
+        angle = parseInt(currentDegree.substring(start + 1, end)) + angle;
     }
     return angle;
 }
