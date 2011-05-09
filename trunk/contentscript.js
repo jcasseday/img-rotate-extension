2379dcab3dd3b5dd7c54461e4ab9e92558d44f2e
abdul2000@gmail.com <abdul2000@gmail.com@5c71e214-9e17-7a57-94e1-b0fcdf2311e2>
2011-05-09 14:15:44 +0000

    fixed problem with rotating images when <img> elements are added straight to <body>

--- trunk/contentscript.js
+++ trunk/contentscript.js
@@ -1,19 +1,14 @@
 var selectedImage = null;
-//If the image is loaded by itself on the web page
-if (document.body.childElementCount === 1) {
-    selectedImage = $(document.images[0]).get(0);
-} else {
-    $("img", document.body).mousedown(function (event) {
-        event.stopPropagation();
-        switch (event.which) {
-            case 3:
-                selectedImage = $(this).get(0);
-                break;
-            default:
-                break;
-        }
-    });
-}
+$("img", document.body).mousedown(function (event) {
+    event.stopPropagation();
+    switch (event.which) {
+        case 3:
+            selectedImage = $(this).get(0);
+            break;
+        default:
+            break;
+    }
+});
 
 chrome.extension.onRequest.addListener(onRequest);
 
